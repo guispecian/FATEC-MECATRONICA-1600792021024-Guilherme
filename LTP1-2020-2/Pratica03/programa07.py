b9bf1b7fbd760951a7d9b957f270d7a634c47a6e
guispecian <70581645+guispecian@users.noreply.github.com>
2020-09-08 18:06:28 -0300

    Create programa07.py

---
+++ LTP1-2020-2/Pratica03/programa07.py
@@ -0,0 +1,5 @@
+#Exercicio 3; valor do volume do cilindro pelo raio e a altura
+raio = float(input('informe o valor do raio do cilindro em metros:'))
+altura = float(input('informe a altura do cilindro em metros:'))
+volume_cilindro = 3.1416 * raio * raio * altura
+print('O volume do clindro:', volume_cilindro, 'm³' )
