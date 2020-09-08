48b1711e439b281b75dab6fa72721e212f590a93
guispecian <70581645+guispecian@users.noreply.github.com>
2020-09-08 17:34:05 -0300

    Create programa05.py

---
+++ LTP1-2020-2/Pratica03/programa05.py
@@ -0,0 +1,4 @@
+#Exercicio 1; graus celsius convertida para kelvin
+temp_celsius = float(input('Informar o valor de graus celsius:'))
+temp_kelvin = temp_celsius + 273.15
+print('O valor de ', temp_celsius, '°C (graus celsius) convertido em Kelvin:', temp_kelvin, ('K'))
