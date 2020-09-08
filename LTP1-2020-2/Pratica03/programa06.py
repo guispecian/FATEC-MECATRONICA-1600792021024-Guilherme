d938e9fab33c82fe507754d9e295a3768e72ad3c
guispecian <70581645+guispecian@users.noreply.github.com>
2020-09-08 17:47:37 -0300

    Create programa06.py

---
+++ LTP1-2020-2/Pratica03/programa06.py
@@ -0,0 +1,4 @@
+#Exercicio 2; graus celsius covertido para graus fahrenheit
+graus_celsius = float(input('informar o valor de graus celsius:'))
+graus_fahrenheit = 1.8 * graus_celsius + 32
+print('o valor de', graus_celsius, '°C (graus celsius) convertido para graus fahrenheit:', graus_fahrenheit, '°F')
