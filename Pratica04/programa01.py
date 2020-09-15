fb00610a5556fdf502d050b7a38567776d225a70
guispecian <70581645+guispecian@users.noreply.github.com>
2020-09-15 08:12:52 -0300

    Create programa01.py

---
+++ Pratica04/programa01.py
@@ -0,0 +1,10 @@
+#Pede ao usuario para digitar um valor para sua idade
+idade = int(input('Informe sua idade'))
+
+#Pergunta, no código, se a idade é maior ou igual a 18
+if idade >= 18:
+  #Este código pertence ao bloco do if 
+  print('Você possui 18 anos ou mais')
+  print('Agora! Você pode ver todas as paginas de jogos da steam')
+
+print('Final do programa')
