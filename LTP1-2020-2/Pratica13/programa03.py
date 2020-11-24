fa1058e75c65fefa54368ab4201f0e3d61b3084f
guispecian <70581645+guispecian@users.noreply.github.com>
2020-11-24 09:42:03 -0300

    Create programa03.py

---
+++ LTP1-2020-2/Pratica13/programa03.py
@@ -0,0 +1,9 @@
+numero1 = float(input('Informe um numero:'))
+numero2 = float(input('Informe outro numero:'))
+
+if numero1 > numero2:
+  print('Maior:', numero1)
+elif numero2 > numero1:
+  print('Maior:', numero2)
+else:
+  print('Os dois são iguais')
